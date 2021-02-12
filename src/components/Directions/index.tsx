16d60381691ffe3ae7e6a7ac105891224e48653a
Fábio Alvarenga <fabio99alvarenga@gmail.com>
2021-02-11 22:11:51 -0300

    feat: add MapViewDirections

---
+++ src/components/Directions/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import MapViewDirections from 'react-native-maps-directions';
+import api from '../../config/api';
+
+interface DirectionsProps {
+	destination: any;
+	origin: any;
+	onReady: any;
+}
+
+const Directions = ({ destination, origin, onReady }: DirectionsProps) => {
+	const [GCP_API_KEY, SET_GCP_API_KEY] = useState('');
+	const [IS_API_KEY_LOADED, SET_IS_API_KEY_LOADED] = useState(false);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const res = await api.get('/GCP_API');
+				if (res.data.msg) {
+					SET_GCP_API_KEY(res.data.msg);
+					SET_IS_API_KEY_LOADED(true);
+				}
+			} catch (err) {
+				SET_IS_API_KEY_LOADED(false);
+			}
+		})();
+	}, []);
+
+	return (
+		<>
+			{IS_API_KEY_LOADED ? (
+				<MapViewDirections
+					destination={destination}
+					origin={origin}
+					onReady={onReady}
+					apikey={GCP_API_KEY}
+					strokeColor='#222'
+					strokeWidth={3}
+				/>
+			) : (
+				// Add a spinner here
+				<View style={{ position: 'absolute' }}>
+					<Text style={{ fontSize: 50 }}>Loading...</Text>
+				</View>
+			)}
+		</>
+	);
+};
